76f76a031fd04c5d13fa439ed1290552352fcd72
Daniel Mayorga <daniel.mayorga97@hotmail.com>
2023-12-07 20:43:12 -0500

    Refactor middleware and update dependencies

--- app.js
+++ app.js
@@ -15,51 +15,40 @@ const authRouter = require('./routes/auth.routes');
 
 const app = express();
 
-// Middleware for CORS
 app.use(cors({ 
   origin: ["http://localhost:3000", "http://localhost:5173", "*"],
   credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   optionsSuccessStatus: 204,
 }));
-
-// Middleware for logging
 app.use(logger('dev'));
-
-// Middleware for parsing JSON and URL-encoded data
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
-
-// Middleware for cookie parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Middleware for serving static files (if your React app is built)
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Routes
 app.use('/', indexRouter);
 app.use('/api/users', userRouter);
 app.use('/api/tickets', ticketRouter);
 app.use('/auth', authRouter);
 
-// Catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// Error handler
+// error handler
 app.use(function (err, req, res, next) {
   res.status(err.status || 500).json({
     error: {
       message: err.message,
-      // Remove the line below in a production environment
       stack: req.app.get('env') === 'development' ? err.stack : undefined,
     },
   });
 });
 
-// Start server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '10mb' }));
+
+module.exports = app;
