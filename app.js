50f05a04610ac22255c9ab8e9e70c69672e1d160
Daniel Mayorga <daniel.mayorga97@hotmail.com>
2023-12-07 21:16:53 -0500

    Add catch-all route for serving index.html file

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.use('/', indexRouter);
 app.use('/api/users', userRouter);
 app.use('/api/tickets', ticketRouter);
 app.use('/auth', authRouter);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
