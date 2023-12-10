4d444559a52afe1e02bfb299066c7ba0a34ad43b
Daniel Mayorga <daniel.mayorga97@hotmail.com>
2023-12-10 10:36:38 -0500

    Update Signin page UI

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -30,15 +30,14 @@ function Signin() {
   };
 
   return (
-    <div className="login">
+    <div className="yourbigdiv">
       <div className="login-signup">
         <div id="background">
           <div id="center">
             <form>
-              <a id="logo" href='/'>
-                <img className="logo" src="/Logo/1.png" width="150" height="150" alt="Logo" />
-              </a>
-              <h3 id="logintext">Sign In</h3>
+            <a id="logo" href='/'><img className="logo" src="/Logo/1.png" width="150" height="150"/></a> 
+      <h3 id="logintext">Sign In</h3>
+            
               <div className="input">
                 <label className="username" id="usernametext">Email:</label>
                 <input
@@ -73,15 +72,22 @@ function Signin() {
               </div>
             </form>
             <svg xmlns="http://www.w3.org/2000/svg" width="600" height="500" viewBox="0 0 713 892" fill="none">
-              <g filter="url(#filter0_i_2_34)">
-                <path d="M0 137.124L348.995 0L713 137.124V743.102L348.995 892L0 743.102V137.124Z" fill="#DBE8CF" />
-              </g>
-              <defs>
-                <filter id="filter0_i_2_34" x="0" y="0" width="713" height="896" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                  {/* ... (rest of the filter definition) */}
-                </filter>
-              </defs>
-            </svg>
+      <g filter="url(#filter0_i_2_34)">
+        <path d="M0 137.124L348.995 0L713 137.124V743.102L348.995 892L0 743.102V137.124Z" fill="#DBE8CF"/>
+      </g>
+      <defs>
+        <filter id="filter0_i_2_34" x="0" y="0" width="713" height="896" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+          <feOffset dy="4"/>
+          <feGaussianBlur stdDeviation="2"/>
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.5 0"/>
+          <feBlend mode="normal" in2="shape" result="effect1_innerShadow_2_34"/>
+        </filter>
+      </defs>
+    </svg>
           </div>
         </div>
       </div>
